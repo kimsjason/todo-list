701f2ad7d707e56324369546d47a99b59e65770f
Jason Kim <jason.s.kim95@gmail.com>
2021-10-27 12:53:25 -0400

    Initial dev configuration

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+                template: './src/template.html'
+            }),
+        new CleanWebpackPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.html$/i,
+                use: ['html-loader'],
+            },
+            {
+                test: /\.(svg|png|jpg|gif)$/i,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'images'
+                    }
+                }
+            }
+        ]
+    }
+}
