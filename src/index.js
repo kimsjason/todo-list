8ae3827593b5e2f691981fef472f2251269b8541
Jason Kim <jason.s.kim95@gmail.com>
2021-10-28 15:19:25 -0400

    Move code to relevant modules

--- src/index.js
+++ src/index.js
@@ -1,27 +1,11 @@
-import { taskForm } from './task-form';
-import { todo, todoItem } from './todo';
-import { domItem } from './dom';
+import { TaskForm } from './task-form';
 import './main.css';
 
-const main = todo();
-
-// example task
-const item = todoItem(
-    'Walk dog', 
-    'Daily Chores', 
-    'Walk ziggy before going to work', 
-    '10/27/2021', 
-    'medium',
-    'incomplete',
-    '#chores'
-    );
-
-main.addItem(item)
-const domElement = domItem(item);
-
+// instantiate new task form
+const taskform = TaskForm();
 const content = document.querySelector('.content');
-const form = taskForm();
-content.appendChild(form);
-content.appendChild(domElement);
+content.appendChild(taskform.taskForm);
 
-//console.log(main.list)
+
+
+
