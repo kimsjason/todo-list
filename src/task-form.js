ebac0ccdeb4e7d9bb74217ffcb589e54721f0b6b
Jason Kim <jason.s.kim95@gmail.com>
2021-10-28 15:21:01 -0400

    Reorganize task form code

--- src/task-form.js
+++ src/task-form.js
@@ -1,77 +1,44 @@
-import { domItem } from "./dom";
-import { todoItem } from "./todo";
+import { DOMTasks } from "./dom";
+import { Task } from "./tasks";
+import { Tasklist } from "./tasks";
+import { Events } from "./events";
 
-const taskForm = () => {
-    // list item element
-    const task = document.createElement('div');
-    task.classList.add('task-form');
-    task.innerHTML = 'New Task';
+const TaskForm = () => {
+    // instantiate task list
+    const tasklist = Tasklist();
+    const dom = DOMTasks();
+    const events = Events();
 
-    // list item property elements
-    const title = document.createElement('input');
-    const projectName = document.createElement('input');
-    const description = document.createElement('input');
-    const dueDate = document.createElement('input');
-    const priority = document.createElement('input');
-    const status = document.createElement('input');
-    const hashtags = document.createElement('input');
+    // example task
+    const example = Task('Walk dog', 'Daily Chores', 'Walk ziggy before going to work', '10/27/2021', 'medium','incomplete','#chores');
+    tasklist.addTask(example)
 
-    title.type = 'text';
-    projectName.type = 'text';
-    description.type = 'text';
-    dueDate.type = 'text';
-    priority.type = 'text';
-    status.type = 'text';
-    hashtags.type = 'text';
+    // create element for new task form
+    const taskForm = document.createElement('div');
+    taskForm.classList.add('task-form');
+    taskForm.innerHTML = 'New Task';
 
-    title.placeholder = 'title';
-    projectName.placeholder = 'project name';
-    description.placeholder = 'description';
-    dueDate.placeholder = 'due date';
-    priority.placeholder = 'priority';
-    status.placeholder = 'status';
-    hashtags.placeholder = '#hashtag';
-
-    title.classList.add('title');
-    projectName.classList.add('project-name');
-    description.classList.add('description');
-    dueDate.classList.add('due-date');
-    priority.classList.add('priority');
-    status.classList.add('status');
-    hashtags.classList.add('hashtags');
-
-    task.appendChild(title);
-    task.appendChild(projectName);
-    task.appendChild(description);
-    task.appendChild(dueDate);
-    task.appendChild(priority);
-    task.appendChild(status);
-    task.appendChild(hashtags);
-
-    // add new task button
-    const addTask = document.createElement('input');
-    addTask.type = 'button';
-    addTask.value = 'Add Task';
-    task.appendChild(addTask);
-
-    // add task event listener
-    addTask.addEventListener('click', () => {
-        const item = todoItem(
-            title.value,
-            projectName.value,
-            description.value,
-            dueDate.value,
-            priority.value,
-            status.value,
-            hashtags.value
-        );
-    
-        const content = document.querySelector('.content');
-        const newTask = domItem(item);
-        content.appendChild(newTask);
+    // create property elements for task form
+    const properties = ['title', 'projectName', 'description', 'dueDate', 'priority', 'complete', 'hashtags'];
+    properties.forEach(property => {
+        const propertyElement = dom.createPropertyElement('input', 'text', property, 'task-form-property', undefined);
+        taskForm.appendChild(propertyElement);
     });
 
-    return task;
+    // add new task submit button
+    const addTaskButton = document.createElement('input');
+    addTaskButton.type = 'button';
+    addTaskButton.value = 'Add Task';
+    taskForm.appendChild(addTaskButton);
+
+    // add task event listener
+    events.addTaskEvent(addTaskButton, tasklist, taskForm);
+    dom.displayTasks(tasklist);
+
+    return {
+        tasklist,
+        taskForm
+    };
 }
 
-export { taskForm };
+export { TaskForm };
