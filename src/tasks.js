134b239a8cae0019506e17f247e5aab6e03a4789
Jason Kim <jason.s.kim95@gmail.com>
2021-11-02 00:35:05 -0400

    Add new tasklist functions - addProject, removeProject

--- src/tasks.js
+++ src/tasks.js
@@ -16,22 +16,24 @@ const Task = (title, projectName, description, dueDate, priority, complete, hash
 
 const Tasklist = () => {
     const tasklist = [];
+    const projects = {};
 
     function createExampleTasks() {
         const examples = [
             Task('Walk dog', 'Daily Chores', 'Walk Ziggy before going to work', new Date(2021, 10, 1), 'medium','incomplete','#chores'),
-            Task('Feed cat', 'Daily Chores', 'Feed Sweetie before studying', new Date(2021, 10, 1), 'high', 'incomplete', '#chores'),
             Task('Complete full stack Javascript', 'Work', 'Complete The Odin Project full stack Javascript course by the end of the year', new Date(2021, 11, 31), 'high', 'incomplete', '#coding'),
-            Task('Review RegEx', 'Work', 'Get familiar with regular expressions', new Date(2021, 8, 20), 'low', 'incomplete', '#coding')
+            Task('Feed cat', 'Daily Chores', 'Feed Sweetie before studying', new Date(2021, 10, 1), 'high', 'incomplete', '#chores'),
+            Task('Review RegEx', 'Work', 'Get familiar with regular expressions', new Date(2021, 8, 20), 'low', 'incomplete', '#coding'),
+            Task('Test', 'Work', 'Testing the project feature', new Date(2021, 10, 3), 'high', 'incomplete', '#ugh')
+
         ];
 
         // sorts examples and adds to tasklist
-        examples.sort((a, b) => (a.dueDate > b.dueDate) ? 1 : -1);
-        examples.forEach(example => addTask(example));
-    }
-    
-    function getTasklist() {
-        return tasklist;
+        //examples.sort((a, b) => (a.dueDate > b.dueDate) ? 1 : -1);
+        examples.forEach(example => {
+            addTask(example);
+            addProject(example.projectName, example);
+        });
     }
     
     function addTask(task) {
@@ -41,6 +43,10 @@ const Tasklist = () => {
     function removeTask(task) {
         const index = tasklist.indexOf(task);
         tasklist.splice(index, 1);
+    }
+
+    function getTasklist() {
+        return tasklist;
     }
 
     function getTodaysTasks() {
@@ -59,13 +65,35 @@ const Tasklist = () => {
         return upcomingTasks;
     }
 
+    function addProject(projectName, task) {
+        if (projects[projectName]) {
+            projects[projectName].push(task);
+        } else {
+            projects[projectName] = [task];
+        }
+    }
+
+    function removeProject(projectName) {
+        delete projects[projectName];
+
+        // for later: remove tasks under this project in tasklist (for loop in this module with removeTask function?)
+    }
+
+    function getProjects() {
+        return projects;
+    }
+
+    
+
     return {
-        getTasklist,
         createExampleTasks,
         addTask,
         removeTask,
+        getTasklist,
         getTodaysTasks,
-        getUpcomingTasks
+        getUpcomingTasks,
+        addProject,
+        getProjects
     }
 }
 
