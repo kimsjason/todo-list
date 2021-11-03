5c13fc81ee02e1f16dd1f66ae3e6cb0394037c42
Jason Kim <jason.s.kim95@gmail.com>
2021-11-03 13:53:14 -0400

    Move task form and examples to respective modules

--- src/index.js
+++ src/index.js
@@ -3,21 +3,15 @@ import { Tasklist } from './tasks';
 import { Events } from './events';
 import './main.css';
 
-// instantiate new task form
+// instantiate Tasklist, DOMTasks, and Events modules
 const tasklist = Tasklist();
-const dom = DOMTasks(tasklist);
-
-// create task form
-const taskform = dom.createTaskForm();
-const content = document.querySelector('.content');
-content.appendChild(taskform);
-
-// initialize example tasks
-tasklist.createExampleTasks();
-
-dom.displayTasks(dom.createExampleTasks(tasklist.getTasklist()));
-
+const dom = DOMTasks();
 Events(dom, tasklist);
+
+
+
+dom.showView(tasklist.getTasklist());
+
 
 
 
