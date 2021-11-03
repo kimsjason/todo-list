492e47f73f18234475876379f4a4e10ab82efa5c
Jason Kim <jason.s.kim95@gmail.com>
2021-11-03 13:58:22 -0400

    Move example tasks in initialization function to tasklist array

--- src/tasks.js
+++ src/tasks.js
@@ -1,4 +1,4 @@
-import { isFuture, isToday } from "date-fns";
+import { isFuture, isToday, parseISO } from "date-fns";
 
 // in future - think about factory function inheritance
 const Task = (title, projectName, description, dueDate, priority, complete, hashtags) => {
@@ -15,27 +15,21 @@ const Task = (title, projectName, description, dueDate, priority, complete, hash
 }
 
 const Tasklist = () => {
-    const tasklist = [];
+    const tasklist = [
+        Task('Walk dog', 'Daily Chores', 'Walk Ziggy before going to work', parseISO('2021-11-03'), 'medium','incomplete','#chores'),
+        Task('Complete full stack Javascript', 'Work', 'Complete The Odin Project full stack Javascript course by the end of the year', parseISO('2021-11-30'), 'high', 'incomplete', '#coding'),
+        Task('Feed cat', 'Daily Chores', 'Feed Sweetie before studying', parseISO('2021-10-02'), 'high', 'incomplete', '#chores'),
+        Task('Review RegEx', 'Work', 'Get familiar with regular expressions', parseISO('2021-08-20'), 'low', 'incomplete', '#coding'),
+        Task('Test', 'Work', 'Testing the project feature', parseISO('2021-10-03'), 'high', 'incomplete', '#ugh')
+    ];
+    
     const projects = {};
 
-    function createExampleTasks() {
-        const examples = [
-            Task('Walk dog', 'Daily Chores', 'Walk Ziggy before going to work', new Date(2021, 10, 1), 'medium','incomplete','#chores'),
-            Task('Complete full stack Javascript', 'Work', 'Complete The Odin Project full stack Javascript course by the end of the year', new Date(2021, 11, 31), 'high', 'incomplete', '#coding'),
-            Task('Feed cat', 'Daily Chores', 'Feed Sweetie before studying', new Date(2021, 10, 1), 'high', 'incomplete', '#chores'),
-            Task('Review RegEx', 'Work', 'Get familiar with regular expressions', new Date(2021, 8, 20), 'low', 'incomplete', '#coding'),
-            Task('Test', 'Work', 'Testing the project feature', new Date(2021, 10, 3), 'high', 'incomplete', '#ugh')
-
-        ];
-
-        // sorts examples and adds to tasklist
-        //examples.sort((a, b) => (a.dueDate > b.dueDate) ? 1 : -1);
-        examples.forEach(example => {
-            addTask(example);
-            addProject(example.projectName, example);
-        });
-    }
+    tasklist.forEach(example => {
+        addProject(example.projectName, example);
+    });
     
+
     function addTask(task) {
         tasklist.push(task);
     }
@@ -83,17 +77,15 @@ const Tasklist = () => {
         return projects;
     }
 
-    
 
     return {
-        createExampleTasks,
         addTask,
         removeTask,
         getTasklist,
         getTodaysTasks,
         getUpcomingTasks,
         addProject,
-        getProjects
+        getProjects,
     }
 }
 
