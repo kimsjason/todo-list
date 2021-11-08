7b9d4752553510fe0aaad59c54b8bfa9d5e1d06a
Jason Kim <jason.s.kim95@gmail.com>
2021-11-07 20:04:16 -0500

    Update dom functions

--- src/dom.js
+++ src/dom.js
@@ -1,41 +1,29 @@
 import { format } from 'date-fns';
 
 const DOMTasks = () => {
+    const sidebar = document.querySelector('.sidebar');
     const content = document.querySelector('.content');
     const taskContent = document.querySelector('.task-content');
-    const properties = ['title', 'project', 'description', 'dueDate', 'priority', 'hashtags'];
-    
-    const sidebar = document.querySelector('.sidebar');
-    const dropdown = document.querySelector('.dropdown');
 
+    const properties = ['title', 'description', 'dueDate', 'project', 'priority'];
+    
     displayTaskForm(createTaskForm())
 
-    function createDiv(classList, innerHTML) {
-        const div = document.createElement('div');
-        div.classList.add(classList);
-        div.innerHTML = innerHTML;
-
-        return div;
-    }
-
-    function createInput(type, classList, value) {
-        const input = document.createElement('input');
-        input.type = type;
-        input.classList.add(classList);
-        input.value = value;
-
-        return input;
-    }
-
-    function updateDropdown(projects) {
-        hideElement(dropdown);
-        //dropdown.classList.toggle('hidden');
+    function createDropdown(projects) {
+        const dropdown = document.querySelector('.dropdown');
+        dropdown.classList.toggle('hidden');
         dropdown.innerHTML = '';
         Object.keys(projects).forEach(project => {
-            const dropdownItems = createDiv('dropdown-items', project);
+            const dropdownItems = document.createElement('div');
+            dropdownItems.classList.add('dropdown-items');
+            dropdownItems.innerHTML = project;
             dropdown.appendChild(dropdownItems);
         });
 
+        return dropdown;
+    }
+
+    function displayDropdown(dropdown) {
         sidebar.appendChild(dropdown);
     }
 
@@ -43,14 +31,22 @@ const DOMTasks = () => {
         const formProperties = [];
         
         properties.forEach(property => {
+            const propertyContainer = document.createElement('div');
+            const propertyName = document.createElement('div');
             const propertyElement = document.createElement('input');
 
+            propertyContainer.classList.add('property-container', property);
+            propertyName.classList.add('property-name');
+            propertyElement.classList.add('task-form-property');
+
+            propertyName.innerHTML = property;
             propertyElement.type = 'text';
             propertyElement.name = property;
-            propertyElement.classList.add('task-form-property');
-            propertyElement.placeholder = property;
-            
-            formProperties.push(propertyElement);
+
+            propertyContainer.appendChild(propertyName);
+            propertyContainer.appendChild(propertyElement);
+
+            formProperties.push(propertyContainer);
         });
 
         return formProperties;
@@ -58,24 +54,51 @@ const DOMTasks = () => {
 
     function createTaskForm() {
         // task form
-        const taskForm = createDiv('task-form', 'New Task');
+        const taskForm = document.createElement('div');
+        taskForm.classList.add('task-form');
 
+        // task form header
+        const taskFormHeader = document.createElement('div');
+        taskFormHeader.classList.add('task-form-header');
+        taskFormHeader.innerHTML = 'New Task';
+        taskForm.appendChild(taskFormHeader);
+
+        // task form content
+        const taskFormContent = document.createElement('div');
+        taskFormContent.classList.add('task-form-content');
+        
         // task form properties
         const formProperties = createFormProperties();
         formProperties.forEach(property => {
-            taskForm.appendChild(property);
+            taskFormContent.appendChild(property);
         });
+        taskForm.appendChild(taskFormContent);
+
+        // task form footer
+        const taskFormFooter = document.createElement('div');
+        taskFormFooter.classList.add('task-form-footer');
+
+        // close task form button
+        const closeTaskButton = document.createElement('input');
+        closeTaskButton.type = 'button';
+        closeTaskButton.classList = 'close-task';
+        closeTaskButton.value = 'Close Task';
+        taskFormFooter.appendChild(closeTaskButton);
 
         // add new task button
-        const addTaskButton = createInput('button', 'add-task', 'Add Task');
-        taskForm.appendChild(addTaskButton);
-        content.appendChild(taskForm);
+        const addTaskButton = document.createElement('input');
+        addTaskButton.type = 'button';
+        addTaskButton.classList.add('add-task');
+        addTaskButton.value = 'Add Task';
+        taskFormFooter.appendChild(addTaskButton)
+        
+        taskForm.appendChild(taskFormFooter);
 
         return taskForm;
     }
 
-    function displayTaskForm(taskform) {
-        content.appendChild(taskform);
+    function displayTaskForm(taskForm) {
+        content.appendChild(taskForm);
     }
 
     function createTaskProperties(task) {
@@ -86,9 +109,9 @@ const DOMTasks = () => {
         mainProperties.classList.add('main-properties');
         detailProperties.classList.add('detail-properties', 'hidden');
 
-
         properties.forEach(property => {
             const propertyElement = document.createElement('div');
+            propertyElement.classList.add(property);
             if (property == 'title') {
                 propertyElement.innerHTML = task[property];
                 mainProperties.appendChild(propertyElement);
@@ -96,8 +119,12 @@ const DOMTasks = () => {
                 propertyElement.innerHTML = format(task[property], 'MM/dd/yyyy');
                 mainProperties.appendChild(propertyElement);
             } else {
-                propertyElement.innerHTML = `${property[0].toUpperCase() + property.slice(1)}: ${task[property]}`;
-                detailProperties.appendChild(propertyElement);
+                const propertyLabel = document.createElement('label');
+                propertyLabel.innerHTML = `${property[0].toUpperCase() + property.slice(1)}:`;
+                propertyElement.innerHTML = task[property];
+
+                propertyLabel.appendChild(propertyElement);
+                detailProperties.appendChild(propertyLabel);
             }
 
             taskProperties.push(mainProperties);
@@ -111,7 +138,7 @@ const DOMTasks = () => {
         const taskProperties = createTaskProperties(task);
 
         // create element for new task
-        const taskElement = document.createElement('li');
+        const taskElement = document.createElement('div');
         taskElement.classList.add('task');
         
         // main content container
@@ -130,39 +157,42 @@ const DOMTasks = () => {
         // add edit and delete icons to container
         const editTaskButton = document.createElement('span');
         const removeTaskButton = document.createElement('span');
+
         editTaskButton.classList.add('material-icons', 'edit-task');
         removeTaskButton.classList.add('material-icons', 'remove-task');
+
         editTaskButton.innerHTML = 'edit';
         removeTaskButton.innerHTML = 'delete_outline';
+
         mainContent.appendChild(editTaskButton);
         mainContent.appendChild(removeTaskButton);
 
         // add main content container to task element
         taskElement.appendChild(mainContent);
+
         // add detailed properties to task element (initially hidden)
         taskElement.appendChild(taskProperties[1]);
 
-        // store object reference in html task element
+        // store task object reference in html task element
         taskElement.taskObject = task;
 
         return taskElement;
     }
     
+    function editTaskElement() {
+
+    }
+
     function createTasks(tasklist) {
         const tasks = document.createElement('div');
         tasks.classList.add('tasks');
         
         tasklist.forEach(task => {
             const taskElement = createTaskElement(task);
-            taskElement.taskObject = task;
             tasks.appendChild(taskElement);
         });
 
         return tasks;
-    }
-
-    function clearTaskDisplay() {
-        taskContent.innerHTML = '';
     }
 
     function displayTasks(tasks) {
@@ -171,30 +201,21 @@ const DOMTasks = () => {
 
     function showView(tasklist) {
         const tasks = createTasks(tasklist);
-        clearTaskDisplay();
+        taskContent.innerHTML = '';
         displayTasks(tasks);
-    }
-    
-    function hideElement(element) {
-        element.classList.toggle('hidden');
-        return element;
     }
 
     return {
-        createDiv,
-        createInput,
-        updateDropdown,
+        createDropdown,
+        displayDropdown,
         createFormProperties,
         createTaskForm,
         displayTaskForm,
         createTaskProperties,
         createTaskElement,
         createTasks,
-        clearTaskDisplay,
         displayTasks,
         showView,
-        hideElement
-    
     };
 }
 
